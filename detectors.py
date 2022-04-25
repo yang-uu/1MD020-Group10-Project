dbe7439cb2b848e65df43ab04592543f300d3273
Yang <yang.yang.3615@student.uu.se>
2022-04-25 19:58:13 +0200

    `ArUcoDetector`: enumerate from all predefined dicts.

--- detectors.py
+++ detectors.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 
 import cv2
+from loguru import logger
 
 class QRCodeDetector:
 
@@ -12,11 +13,37 @@ class QRCodeDetector:
         return detectedText, points
 
 
-class ArUcoDetector:
+class DumbArUcoDetector:
 
-    def __init__(self, type_):
-        self.dict = cv2.aruco.Dictionary_get(getattr(cv2.aruco, type_))
+    PREDIFINED_DICT_NAME = {
+        cv2.aruco.DICT_4X4_50: "DICT_4X4_50",
+        cv2.aruco.DICT_4X4_100: "DICT_4X4_100",
+        cv2.aruco.DICT_4X4_250: "DICT_4X4_250",
+        cv2.aruco.DICT_4X4_1000: "DICT_4X4_1000",
+        cv2.aruco.DICT_5X5_50: "DICT_5X5_50",
+        cv2.aruco.DICT_5X5_100: "DICT_5X5_100",
+        cv2.aruco.DICT_5X5_250: "DICT_5X5_250",
+        cv2.aruco.DICT_5X5_1000: "DICT_5X5_1000",
+        cv2.aruco.DICT_6X6_50: "DICT_6X6_50",
+        cv2.aruco.DICT_6X6_100: "DICT_6X6_100",
+        cv2.aruco.DICT_6X6_250: "DICT_6X6_250",
+        cv2.aruco.DICT_6X6_1000: "DICT_6X6_1000",
+        cv2.aruco.DICT_7X7_50: "DICT_7X7_50",
+        cv2.aruco.DICT_7X7_100: "DICT_7X7_100",
+        cv2.aruco.DICT_7X7_250: "DICT_7X7_250",
+        cv2.aruco.DICT_7X7_1000: "DICT_7X7_1000",
+        cv2.aruco.DICT_ARUCO_ORIGINAL: "DICT_ARUCO_ORIGINAL",
+        cv2.aruco.DICT_APRILTAG_16h5: "DICT_APRILTAG_16h5",
+        cv2.aruco.DICT_APRILTAG_25h9: "DICT_APRILTAG_25h9",
+        cv2.aruco.DICT_APRILTAG_36h10: "DICT_APRILTAG_36h10",
+        cv2.aruco.DICT_APRILTAG_36h11: "DICT_APRILTAG_36h11",
+    }
 
     def detect(self, img_array):
-        points, detectedText, _ = cv2.aruco.detectMarkers(img_array, self.dict)
+        detectedText, points = None, ()
+        for id_, name in self.PREDIFINED_DICT_NAME.items():
+            results = cv2.aruco.detectMarkers(img_array, cv2.aruco.Dictionary_get(id_))
+            if len(results[0]) > 0:
+                points, detectedText, _ = results
+                logger.debug(f"Found with dict `{name}`.")
         return detectedText, points
--- examples.py
+++ examples.py
@@ -1,15 +1,15 @@
 # coding: utf-8
 
 from main import detect_image
-from detectors import QRCodeDetector, ArUcoDetector
+from detectors import QRCodeDetector, DumbArUcoDetector
 
 if __name__ == "__main__":
     print("Detect `qrcode-1.jpg`...")
-    text, points, time_used = detect_image("./images/qrcode-1.jpg", QRCodeDetector())
-    print(f"Results: {text=}, {points=}")
-    print(f"Time used: {time_used}")
+    text, points, creteria = detect_image("./images/qrcode-1.jpg", QRCodeDetector())
+    print(f"Results:\n  {text=},\n  {points=}")
+    print(f"Time used: {creteria}")
 
     print("Detect `ArUco_6x6_1000-711.jpg`...")
-    text, points, time_used = detect_image("./images/ArUco_6x6_1000-711.jpg", ArUcoDetector("DICT_6X6_1000"))
-    print(f"Results: {text=}, {points=}")
-    print(f"Time used: {time_used}")
+    text, points, creteria = detect_image("./images/ArUco_6x6_1000-711.jpg", DumbArUcoDetector())
+    print(f"Results:\n  {text=},\n  {points=}")
+    print(f"Time used: {creteria}")
